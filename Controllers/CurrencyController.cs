b0ed5aae3fe2988738d39f5a5748ee73056c6d9a
akifisitan <102677971+akifisitan@users.noreply.github.com>
2023-08-28 20:50:25 +0300

    Remove unnecessary auth requirement

--- Controllers/CurrencyController.cs
+++ Controllers/CurrencyController.cs
@@ -23,7 +23,6 @@ namespace RealEstateApp.Api.Controllers
             _set = _context.Currencies;
         }
 
-        [Authorize(Roles = UserRoles.User)]
         [HttpGet]
         [Route("list")]
         public async Task<IActionResult> GetAll()

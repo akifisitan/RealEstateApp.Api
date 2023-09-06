068ddbf195badee217b141610e0624006eec79ac
akifisitan <102677971+akifisitan@users.noreply.github.com>
2023-09-07 01:29:43 +0300

    Add endpoint to list users

--- Controllers/AuthenticateController.cs
+++ Controllers/AuthenticateController.cs
@@ -6,6 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 using RealEstateApp.Api.Auth;
 using RealEstateApp.Api.DatabaseContext;
 using RealEstateApp.Api.DTO.AuthDTO;
+using RealEstateApp.Api.DTO.UserDTO;
 using RealEstateApp.Api.Entity;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -34,6 +35,26 @@ namespace RealEstateApp.Api.Controllers
             _configuration = configuration;
             _context = context;
         }
+
+        [HttpGet]
+        [Authorize(Roles = UserRoles.Admin)]
+        [Route("users/list")]
+        public async Task<IActionResult> List()
+        {
+            var result = await _context.Users.AsNoTracking().ToListAsync();
+            var users = new List<UserListDTO>();
+            foreach (var user in result)
+            {
+                users.Add(new UserListDTO()
+                {
+                    Id = user.Id,
+                    Username = user.Username,
+                    Email = user.Email
+                });
+            }
+            return Ok(users);
+        }
+
         [HttpPost]
         [Route("login")]
         public async Task<IActionResult> Login([FromBody] LoginRequestDTO request)
---
+++ DTO/UserDTO/UserListDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstateApp.Api.DTO.UserDTO
+{
+    public class UserListDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+    }
+}
